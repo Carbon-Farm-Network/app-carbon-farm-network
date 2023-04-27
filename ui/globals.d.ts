7437af806e49d404b1713d8efabfdd8a73a79b71
pospi <pospi@spadgos.com>
2023-04-27 19:34:48 +1000

    remove unneeded type, can import from VF-GraphQL now

--- ui/globals.d.ts
+++ ui/globals.d.ts
@@ -1,6 +1,3 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-
-
-export type Agent { latLng: [number]; popup?: string; name: string; iconUrl?: string; address: string; imageUrl?: string }
