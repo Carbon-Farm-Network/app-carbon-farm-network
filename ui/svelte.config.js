fe44b7028681050f35e4d9c7a02de3c9a727abf2
pospi <pospi@spadgos.com>
2023-05-03 18:12:32 +1000

    heavily alter build system to replicate create-svelte-app generator configuration for UI

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,22 +1,16 @@
-import sveltePreprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.svx', '.md'],
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    sveltePreprocess({
-      postcss: true
-    })
-  ],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
   kit: {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: adapter({
-      precompress: true
-    }),
+    adapter: adapter(),
     files: {
       assets: 'static'
     }
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-        plugins: [sveltekit()],
+export default defineConfig({
+	plugins: [sveltekit()],
         define: {
                 'process.env': process.env
-        }
-};
-
-export default config;
+        },
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
