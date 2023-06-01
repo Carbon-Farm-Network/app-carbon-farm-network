a79efebdf4f9f9c8f71aeba0d4e4541579ef3271
pospi <pospi@spadgos.com>
2023-06-01 22:47:29 +1000

    implement zome API bindings for Facet DNA to GraphQL resolver callbacks

--- ui/src/lib/graphql/extension-resolvers.ts
+++ ui/src/lib/graphql/extension-resolvers.ts
@@ -7,93 +7,43 @@
  * @package Carbon Farm Network
  * @since   2023-05-24
  */
+import type { CellId } from '@holochain/client'
+import { mapZomeFn } from '@valueflows/vf-graphql-holochain'
+import type { DNAIdMappings } from '@valueflows/vf-graphql-holochain'
 
-import { AppWebsocket, CellType } from '@holochain/client'
+import type { Organization } from '@valueflows/vf-graphql'
 import type {
-  DNAIdMappings,
-} from '@valueflows/vf-graphql-holochain'
-import type {
-  Organization,
-} from '@valueflows/vf-graphql'
-
-import type {
-  FacetValueOptions,
-  FacetResponse, FacetConnection,
+  FacetValueParams,
+  FacetResponse, Facet,
 } from './extension-schemas'
 
-// per @valueflows/vf-graphql-holochain core
-const ENV_HOLOCHAIN_APP_ID = process.env.REACT_APP_HC_APP_ID as string || ''
-
-// :SHONK: Holochain cell discovery / connection logic
-async function connectFacetsCell(conn: AppWebsocket) {
-  const { dnaConfig } = await sniffHolochainAppCells(conn)
+// key names here are auto-matched by Cell role name convention in `happ.yaml`
+export type ExtendedDnaConfig = DNAIdMappings & {
+  facets: CellId
 }
 
-export async function sniffHolochainAppCells(conn: AppWebsocket, appId?: string) {
-  // use the default set by the environment variable
-  // and furthermore, note that both of these will be ignored
-  // in the Holochain Launcher context
-  // which will override any given value to the AppWebsocket
-  // for installed_app_id
-  appId = appId || ENV_HOLOCHAIN_APP_ID
-  const appInfo = await conn.appInfo({ installed_app_id: appId })
-  if (!appInfo) {
-    throw new Error(`appInfo call failed for Holochain app '${appId}' - ensure the name is correct and that the app installation has succeeded`)
-  }
-
-  let dnaConfig: DNAIdMappings = {}
-  Object.entries(appInfo.cell_info).forEach(([roleName, cellInfos]) => {
-    // this is the "magic pattern" of having for
-    // example the "agreement" DNA, it should have
-    // an assigned "role_name" in the happ of
-    // "hrea_agreement_1" or "hrea_observation_2"
-    // and the middle section should match the expected name
-    // for DNAIdMappings, which are also used during zome calls
-    const cell_match = roleName.match(/^hc_facets$/)
-    if (!cell_match) return
-    // @ts-ignore
-    const firstCell = cellInfos[0]
-    if (CellType.Provisioned in firstCell) {
-      dnaConfig['facets'] = firstCell[CellType.Provisioned].cell_id
-    }
-  })
-
-  console.info('Connecting to detected Holochain facets cell:', dnaConfig)
-
-  return {
-    dnaConfig,
-    appId,
-  }
+export interface ByIdentifier {
+  identifier: string
 }
 
-export default async (/* :TODO: pass Holochain websocket connection here */) => {
-  // :TODO: connect to Holochain facets module
-  const facetsZome = await connectFacetsCell(/* :TODO: pass Holochain websocket connection here */)
+const bindResolvers = async (dnaConfig: ExtendedDnaConfig, conductorUri: string) =>
+{
+  const runCreate = mapZomeFn<FacetValueParams, FacetResponse>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'create_facet_value')
+  const readFacetValues = mapZomeFn<ByIdentifier, Facet[]>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'retrieve_facet_values')
 
   // return all resolvers for GraphQL engine
   return {
     Mutation: {
-      createFacet: async function (_root: any, args: { facet: FacetValueOptions }): FacetResponse {
-        const result = await facetsZome.callZome(/* standard Holochain args */{
-          zome_name: 'facets',
-          fn_name: 'create_facet_or_evs',
-          payload: args.facet,
-        }) as FacetResponse
-        return result
+      putFacetValue: async function (_root: any, args: FacetValueParams): Promise<FacetResponse> {
+        return runCreate(args)
       },
     },
     Organization: {
-      facets: async function (record: Organization, args: { first: number, after: string, last: number, before: string }): FacetConnection {
-        const result = await facetsZome.callZome(/* :TODO: implement */{
-          zome_name: 'facets',
-          fn_name: 'get_facet_values_for_facet_option', // or whichever it is for `getFacetValues` in API spec
-          payload: {
-            ...args,
-            identifier: record.id,
-          },
-        }) as FacetConnection
-        return result
+      facets: async function (record: Organization): Promise<Facet[]> {
+        return readFacetValues({ identifier: record.id })
       },
     },
   }
 }
+
+export default bindResolvers
