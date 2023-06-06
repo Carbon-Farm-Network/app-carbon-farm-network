996fc4c9aeeae43e445fe7a8f38f9e8160c73850
pospi <pospi@spadgos.com>
2023-06-06 16:04:30 +1000

    more impls for Facets resolvers and schema defs. TODOs noted in comments.

--- ui/src/lib/graphql/extension-resolvers.ts
+++ ui/src/lib/graphql/extension-resolvers.ts
@@ -7,14 +7,16 @@
  * @package Carbon Farm Network
  * @since   2023-05-24
  */
-import type { CellId } from '@holochain/client'
+import { encodeHashToBase64, decodeHashFromBase64, type CellId, type EntryHash } from '@holochain/client'
 import { mapZomeFn } from '@valueflows/vf-graphql-holochain'
 import type { DNAIdMappings } from '@valueflows/vf-graphql-holochain'
 
-import type { Organization } from '@valueflows/vf-graphql'
+import type { Organization, ResourceSpecification } from '@valueflows/vf-graphql'
 import type {
-  FacetValueParams,
-  FacetResponse, Facet,
+  FacetGroup, FacetOption, FacetValue,
+  FacetGroupParams, FacetOptionParams, FacetValueParams,
+  FacetGroupResponse, FacetOptionResponse, FacetValueResponse,
+  RawRecordIdentifierMeta,
 } from './extension-schemas'
 
 // key names here are auto-matched by Cell role name convention in `happ.yaml`
@@ -26,23 +28,125 @@ export interface ByIdentifier {
   identifier: string
 }
 
+export interface ByEntryHash {
+  hash: EntryHash
+}
+
+function encodeIdentifiers<T>(data: RawRecordIdentifierMeta & T): T {
+  return {
+    ...data,
+    id: encodeHashToBase64(data.id),
+    revisionId: encodeHashToBase64(data.revisionId),
+  }
+}
+
+// :SHONK: Apologies for the ts-ignores but TypeScript is laborious and dumb at API bindings for little safety gains anyway
+//
 const bindResolvers = async (dnaConfig: ExtendedDnaConfig, conductorUri: string) =>
 {
-  const runCreate = mapZomeFn<FacetValueParams, FacetResponse>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'create_facet_value')
-  const readFacetValues = mapZomeFn<ByIdentifier, Facet[]>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'retrieve_facet_values')
+  // zome write API
+  const runCreateGroup = mapZomeFn<FacetGroupParams, FacetGroupResponse>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'create_facet_group')
+  const runCreateOption = mapZomeFn<FacetOptionParams, FacetOptionResponse>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'create_facet_option')
+  const runCreateValue = mapZomeFn<FacetValueParams, FacetValueResponse>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'create_facet_value')
 
-  // return all resolvers for GraphQL engine
+  // :TODO: deletions API
+
+  // zome read API
+  const readFacetGroups = mapZomeFn<Record<string, never>, FacetGroup[]>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'retrieve_facet_groups')
+  const readFacetOptions = mapZomeFn<ByEntryHash, FacetOption[]>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'get_facet_options_for_facet_group')
+  const readFacetValues = mapZomeFn<ByEntryHash | ByIdentifier, FacetValue[]>(dnaConfig, conductorUri, 'facets', 'hc_facets', 'retrieve_facet_values')
+
+  // declare and return all resolver callbacks for GraphQL engine
   return {
     Mutation: {
-      putFacetValue: async function (_root: any, args: FacetValueParams): Promise<FacetResponse> {
-        return runCreate(args)
+      putFacetGroup: async function (_root: any, args: FacetGroupParams): Promise<FacetGroupResponse> {
+        const res = await runCreateGroup(args)
+        // @ts-ignore
+        res.facetGroup = encodeIdentifiers<FacetGroup>(res.facetGroup)
+        return res
+      },
+      putFacet: async function (_root: any, args: FacetOptionParams): Promise<FacetOptionResponse> {
+        const res = await runCreateOption({
+          ...args,
+          facetGroupId: decodeHashFromBase64(args.facetGroupId),
+        })
+        res.facetOption = {
+          // @ts-ignore
+          ...encodeIdentifiers<FacetOption>(res.facetOption),
+          // @ts-ignore
+          facetGroupId: encodeHashToBase64(res.facetOption.facetGroupId),
+        }
+        return res
+      },
+      putFacetValue: async function (_root: any, args: FacetValueParams): Promise<FacetValueResponse> {
+        const res = await runCreateValue({
+          ...args,
+          facetOptionId: decodeHashFromBase64(args.facetOptionId),
+        })
+        res.facetValue = {
+          // @ts-ignore
+          ...encodeIdentifiers<FacetValue>(res.facetValue),
+          // @ts-ignore
+          facetOptionId: encodeHashToBase64(res.facetValue.facetOptionId),
+        }
+        return res
+      },
+      // :TODO: delete APIs & resolvers
+      // I suspect you can parameterise the API like
+      //     deleteFacetValue: async function (_root: any, args: { identifier: string, value: string }): Promise<bool>
+      // instead of adding the previously proposed `associationId` field to `FacetValue`.
+    },
+    Query: {
+      facetGroups: async (_root: unknown, args: Record<string, never>): Promise<FacetGroup[]> => {
+        const res = await readFacetGroups(args)
+        // :TODO: presuming you are returning from the zome API as a flat array here, but adjust accordingly
+        // @ts-ignore
+        return res.map(encodeIdentifiers)
+      },
+    },
+    FacetGroup: {
+      options: async function (record: FacetGroup): Promise<FacetOption[]> {
+        // :TODO: not sure if this kind of reverse filtering is implemented in the API but is needed for a complete impl
+        const res = await readFacetOptions({ group_hash: decodeHashFromBase64(record.id) })
+        // @ts-ignore
+        return res.map(encodeIdentifiers)
+      },
+    },
+    FacetOption: {
+      group: async function (record: FacetOption): Promise<FacetGroup> {
+        // :TODO: not sure if this kind of reverse filtering is implemented in the API but is needed for a complete impl
+        const res = await readFacetGroups({ has_option_hash: decodeHashFromBase64(record.id) })
+        // @ts-ignore
+        return encodeIdentifiers<FacetGroup>(res.pop() as FacetGroup)
+      },
+      values: async function (record: FacetOption): Promise<FacetValue[]> {
+        const res = await readFacetValues({ option_hash: decodeHashFromBase64(record.id) })
+        // @ts-ignore
+        return res.map(encodeIdentifiers)
+      },
+    },
+    FacetValue: {
+      option: async function (record: FacetValue): Promise<FacetOption> {
+        // :TODO: not sure if this kind of reverse filtering is implemented in the API but is needed for a complete impl
+        const res = await readFacetOptions({ has_value_hash: decodeHashFromBase64(record.id) })
+        // @ts-ignore
+        return encodeIdentifiers<FacetGroup>(res.pop() as FacetOption)
       },
     },
     Organization: {
-      facets: async function (record: Organization): Promise<Facet[]> {
-        return readFacetValues({ identifier: record.id })
+      facets: async function (record: Organization): Promise<FacetValue[]> {
+        const res = await readFacetValues({ identifier: record.id })
+        // @ts-ignore
+        return res.map(encodeIdentifiers)
       },
     },
+    ResourceSpecification: {
+      facets: async function (record: ResourceSpecification): Promise<FacetValue[]> {
+        const res = await readFacetValues({ identifier: record.id })
+        // @ts-ignore
+        return res.map(encodeIdentifiers)
+      },
+    }
   }
 }
 
--- ui/src/lib/graphql/extension-schemas.ts
+++ ui/src/lib/graphql/extension-schemas.ts
@@ -1,68 +1,111 @@
-import type { 
-  ActionHash,
+import type {
+  ActionHashB64, EntryHashB64,
+  EntryHash, ActionHash,
 } from '@holochain/client';
 
-export interface FacetGroup { 
-  group_id: string;
+// zome API creation payloads
+
+export interface FacetGroupParams {
+  name: string;
   note: string;
 }
 
-export interface FacetOption { 
-  facet_id: string;
-  option: string;
-  facet_group: ActionHash | undefined;
+export interface FacetOptionParams {
+  facetGroupId: EntryHashB64
+  option: string
 }
 
-export interface FacetValue { 
-  facet_value: string;
-  facet_option: ActionHash;
-  record_type: string | undefined;
+export interface FacetValueParams {
+  facetOptionId: EntryHashB64
+  value: string // must match a stored `FacetOption.option`
+  note: string
 }
 
+// zome API record types
 
+interface RecordIdentifierMeta {
+  id: EntryHashB64  // EntryHash from `hash_entry`
+  revisionId: ActionHashB64 // ActionHash from `create_entry`
+}
 
-// export interface Facet {
-//   // :TODO:
-// }
+export interface RawRecordIdentifierMeta {
+  id: EntryHash  // EntryHash from `hash_entry`
+  revisionId: ActionHash // ActionHash from `create_entry`
+}
 
-// export interface FacetOptions {
-//   facet_id: string
-//   value: string
-// }
+export type FacetGroup = FacetGroupParams & RecordIdentifierMeta
+export type FacetOption = FacetOptionParams & RecordIdentifierMeta
+export type FacetValue = FacetValueParams & RecordIdentifierMeta
 
-// export interface FacetValueOptions {
-//   facet_eh: Uint8Array
-//   value: string
-//   note: string
-// }
+// mutation API output structs
 
-// export interface FacetValueParams {
-//   facetValue: FacetValueOptions
-// }
+export interface FacetGroupResponse {
+  facetGroup: FacetGroup
+}
 
-// export interface FacetResponse {
-//   facet: Facet
-// }
+export interface FacetOptionResponse {
+  facetOption: FacetOption
+}
+
+export interface FacetValueResponse {
+  facetValue: FacetOption
+}
+
+// :TODO: The below needs to be updated to marry with above TypeScript declarations
+//        and spec in https://github.com/Carbon-Farm-Network/holochain-facets/issues/1
 
 export default [`
-type Facet {
-  # :TODO:
+input FacetGroupParams {
+  group_id: ID!
+  note: string
+}
+
+type FacetGroup {
+  id: ID!
+  revisionId: ID!
+  name: String!
+  note: String
+  options: [FacetOption!]!
+}
+
+type FacetOption {
+  id: ID!
+  revisionId: ID!
+  group: FacetGroup!
+  name: String!
+  note: String
+  values: [FacetValue!]!
+}
+
+type FacetValue {
+  id: ID!
+  revisionId: ID!
+  option: FacetOption!
+  value: String!
+  note: String
 }
 
 input FacetCreateParams {
   # :TODO:
 }
 
-type FacetResponse {
-  facet: Facet
+type FacetOptionResponse {
+  facet: FacetOption
 }
 
 type Organization implements Agent {
   facets(): Facet[]
 }
 
+type ResourceSpecification {
+  facets(): Facet[]
+}
+
 type Mutation  {
+  "Create a new facet grouping."
+  putFacetGroup(facetGroup: FacetGroupParams!): FacetGroupResponse!
+
   "Create a new facet classification."
-  createFacet(facet: FacetCreateParams!): FacetResponse!
+  putFacetOption(facet: FacetOptionCreateParams!): FacetOptionResponse!
 }
 `]
