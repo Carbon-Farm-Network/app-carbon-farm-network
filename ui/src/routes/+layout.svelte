add82b0de698759d96a46778e0dcbb1db7ce517f
happysalada <raphael@megzari.com>
2023-08-29 15:22:53 +0800

    remove holochain loading

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,41 +1,29 @@
 <script lang="ts">
-import { browser } from '$app/environment'
-import { setClient } from 'svelte-apollo'
+  import { browser } from '$app/environment'
+  import { setClient } from 'svelte-apollo'
 
-import "../global.css"
-import { page } from '$app/stores'
+  import '../global.css'
+  import { page } from '$app/stores'
 
-import Initialize from '$lib/Initialize.svelte'
-import Nav from '$lib/Nav.svelte'
+  // import Initialize from '$lib/Initialize.svelte'
+  import Nav from '$lib/Nav.svelte'
 
-/** @type {import('./$types').LayoutData} */
-export let data: any;
-if (browser) {
-  setClient(data.client)
-}
+  /** @type {import('./$types').LayoutData} */
+  export let data: any
+  // if (browser) {
+  //   setClient(data.client)
+  // }
 </script>
 
-{#if !data || !data.client}
-  <section class="overflow-x-hidden w-screen relative">
-    <div class="fade-in h-screen flex flex-col items-center justify-center">
-      <h1 class="font-sans text-white sm:text-xl lg:text-3xl">
-        loader here?
-      </h1>
-    </div>
-  </section>
-{:else}
-  <div class="grid">
-    <nav class="nav">
-      <Nav path={$page.url.pathname} />
-    </nav>
+<div class="grid">
+  <nav class="nav">
+    <Nav path={$page.url.pathname} />
+  </nav>
 
-    <main class="main h-full">
-      <Initialize>
-        <slot />
-      </Initialize>
-    </main>
-  </div>
-{/if}
+  <main class="main h-full">
+    <slot />
+  </main>
+</div>
 
 <style>
   /* minmax on the nav is used for expansion on mobile */
@@ -45,7 +33,7 @@ if (browser) {
     grid-template-rows: 10vh minmax(90vh, auto);
     grid-template-areas:
       'nav'
-      'main'
+      'main';
   }
 
   .nav {
@@ -55,5 +43,4 @@ if (browser) {
   .main {
     grid-area: main;
   }
-
 </style>
--- ui/src/routes/+layout.ts
+++ ui/src/routes/+layout.ts
@@ -1,34 +1,34 @@
-import { browser } from '$app/environment'
-import { error } from '@sveltejs/kit'
+// import { browser } from '$app/environment'
+// import { error } from '@sveltejs/kit'
 // import autoConnect from '@vf-ui/graphql-client-mock' // enable to use in-memory mock API
-import autoConnect from '@vf-ui/graphql-client-holochain'
-import { sniffHolochainAppCells } from '@valueflows/vf-graphql-holochain'
-import { AppWebsocket } from '@holochain/client'
+// import autoConnect from '@vf-ui/graphql-client-holochain'
+// import { sniffHolochainAppCells } from '@valueflows/vf-graphql-holochain'
+// import { AppWebsocket } from '@holochain/client'
 
-import extensionSchemas from '$lib/graphql/extension-schemas'
-import bindResolvers from '$lib/graphql/extension-resolvers'
-import type { ExtendedDnaConfig } from '$lib/graphql/extension-resolvers'
+// import extensionSchemas from '$lib/graphql/extension-schemas'
+// import bindResolvers from '$lib/graphql/extension-resolvers'
+// import type { ExtendedDnaConfig } from '$lib/graphql/extension-resolvers'
 
-const ENV_CONNECTION_URI = process.env.REACT_APP_HC_CONN_URL as string || ''
+// const ENV_CONNECTION_URI = process.env.REACT_APP_HC_CONN_URL as string || ''
 
-export async function load() {
-  if (!browser) return
-  try {
-    // pull DNA config separately in order to bind to CFN-specific extension Cells
-    const conn = await AppWebsocket.connect(ENV_CONNECTION_URI)
-    const { dnaConfig } = await sniffHolochainAppCells(conn)
+// export async function load() {
+//   if (!browser) return
+//   try {
+//     // pull DNA config separately in order to bind to CFN-specific extension Cells
+//     const conn = await AppWebsocket.connect(ENV_CONNECTION_URI)
+//     const { dnaConfig } = await sniffHolochainAppCells(conn)
 
-    return {
-      client: await autoConnect({
-        // passthrough detected DNA config to skip unnecessary redetect
-        dnaConfig,
-        // bind extension GraphQL schema defs and bound resolver callbacks
-        extensionSchemas,
-        extensionResolvers: await bindResolvers(dnaConfig as ExtendedDnaConfig, ENV_CONNECTION_URI),
-      }),
-    }
-  } catch (e) {
-    console.error("Holochain connection error", e)
-    throw error(500, "Holochain connection error")
-  }
-}
+//     return {
+//       client: await autoConnect({
+//         // passthrough detected DNA config to skip unnecessary redetect
+//         dnaConfig,
+//         // bind extension GraphQL schema defs and bound resolver callbacks
+//         extensionSchemas,
+//         extensionResolvers: await bindResolvers(dnaConfig as ExtendedDnaConfig, ENV_CONNECTION_URI),
+//       }),
+//     }
+//   } catch (e) {
+//     console.error("Holochain connection error", e)
+//     throw error(500, "Holochain connection error")
+//   }
+// }
