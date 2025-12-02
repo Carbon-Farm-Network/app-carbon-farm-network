d7c6d3cbdd2ade9212b5d765fe42ef10d91055c0
Leo <leobensman@gmail.com>
2025-12-02 21:43:26 +0100

    fixes to 0.6 upgrade

--- flake.nix
+++ flake.nix
@@ -21,13 +21,14 @@
           hc
           bootstrap-srv
           lair-keystore
-          hc-launch
           hc-scaffold
           hn-introspect
           rust # For Rust development, with the WASM target included for zome builds
         ]) ++ (with pkgs; [
           nodejs_22 # For UI development
+          yarn # Package manager for Node.js
           binaryen # For WASM optimisation
+          typescript # TypeScript compiler for GraphQL adapter
           # Add any other packages you need here
         ]);
 
